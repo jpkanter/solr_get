1bd6e2b22c279a882b151baacac09f6e2f020cb1
JP Kanter <kanter@ub.uni-leipzig>
2020-05-25 10:49:30 +0200

    Changed Download mode to chunks

--- firsttest.py
+++ firsttest.py
@@ -1,5 +1,7 @@
 # global python packages
+import copy
 import json
+import math
 import os
 
 import requests
@@ -13,37 +15,56 @@ from local_tools import is_dict, is_dictkey
 
 # global variables for later use
 # probably bad practice on my part
-main_url = ""
-main_url_param = {}
-error = {}
-structure = {}
+MAIN_URL = ""
+MAIN_URL_PARAM = {}
+CHUNK_SIZE = 50
+ERROR = {}
+STRUCTURE = {}
 statistic = {}
-start_time = time.time() # time for execution time
+start_time = time.time()  # time for execution time
+
 
 # load config data
 
+# error message handler, requires load config before, else it will do just send message
+def send_error(message, error_name=None):
+    global ERROR
+    if error_name is None:
+        sys.stderr.write(message)
+    else:
+        if is_dictkey(ERROR, error_name):
+            sys.stderr.write(ERROR[error_name].format(message))
+        else:
+            sys.stderr.write(message)
+
+
 def load_config(file_path="settings.json"):
-    global main_url, main_url_param, error, structure
+    global MAIN_URL, MAIN_URL_PARAM, ERROR, STRUCTURE, CHUNK_SIZE
     with open(file_path) as json_file:
         data = json.load(json_file)
-        try: error = data['errors']
+        try:
+            ERROR = data['errors']
         except KeyError:
-            print("Cannot find 'error' Listings in {} File".format(file_path))
+            send_error("Cannot find 'error' Listings in {} File".format(file_path))  # in this there is not error field
             sys.exit()
 
-        try: structure = data['structure']
+        try:
+            STRUCTURE = data['structure']
         except KeyError:
-            print(error['key'].format("structure"))
+            send_error("structure", "key")
             sys.exit()
 
         if is_dictkey(data, "url"):
-            main_url = data['url']
-            main_url_param = data['url_para']
+            MAIN_URL = data['url']
+            MAIN_URL_PARAM = data['url_para']
+
+        if is_dictkey(data, "chunk_size"):
+            CHUNK_SIZE = int(data['chunk_size'])
 
 
 # directly requests from solr
 def load_remote_content(url, params):
-    time1 =  time.time()
+    time1 = time.time()
     try:
         resp = requests.get(url, params=params)
         print("Trying URL {}".format(resp.url))
@@ -54,16 +75,16 @@ def load_remote_content(url, params):
         print("Remote load took {} seconds".format(str(round(time.time() - time1, 3))))
         return resp.text
     except requests.exceptions.RequestException as e:
-        print(error['file'].format(e))
+        send_error(e, "file")
 
 
 def test_json(json_str):
-    global error
+    global ERROR
     try:
         data = json.loads(json_str)
         return data
     except ValueError:
-        print(error['json'])
+        print(ERROR['json'])
         return False
 
 
@@ -71,23 +92,26 @@ def traverse_json_data(data):
     # all this does ist to traverse the json to the point where the entries are
     # which is in this hardcoded case the second level in a structure defined by
     # the setting structure['content'], in my cases its called "docs"
-    global structure
+    global STRUCTURE
     i = 0
     for (key, value) in data.items():
-        if key == structure['header']:
+        if key == STRUCTURE['header']:
             header = value
             continue
-        elif key == structure['body'] and is_dict(value):  # next iteration, we know this is a dataset
+        elif key == STRUCTURE['body'] and is_dict(value):  # next iteration, we know this is a dataset
             for (key2, value2) in value.items():
-                if key2 != structure['content']:
+                if key2 != STRUCTURE['content']:
                     continue
                 else:
                     for value3 in enumerate(value2):
                         add_entry(value3)
 
 
+debug_json = []
+
+
 def add_entry(entry):
-    global statistic, error
+    global statistic, ERROR, debug_json
     try:
         for key, value in entry[1].items():  # tuples start at 1, urg
             # the actual entries are actually tuples and dont really possess a key and value like a dict does
@@ -96,8 +120,9 @@ def add_entry(entry):
             else:
                 statistic[key] = 1
             # print("{} = {}".format(key, str(value)))
+        debug_json.append(entry[1])
     except TypeError:
-        print(error['typeerror'].format("add_entry"))
+        send_error("add_entry", "typeerror")
 
 
 def write_statistic(file_path="stats.json"):
@@ -109,22 +134,35 @@ def write_statistic(file_path="stats.json"):
 
         print("{} Bytes of Data were written to {}".format(os.stat(file_path).st_size, file_path))
     except FileExistsError:
-        print("")
+        send_error("", "FileExistsError")
     except FileNotFoundError:
-        print("")  # why should this ever happen?
+        send_error("", "FileNotFoundError")
 
 
 def main():
     load_config()  # default path should suffice
-    temp_json = load_remote_content(main_url, main_url_param)
-    data = test_json(temp_json)
-    if data:  # no else required, test_json already gives us an error if something fails
-        traverse_json_data(data)
-        write_statistic()
-
+    # mechanism to not load 50000 entries in one go but use chunks for it
+    n = math.floor(int(MAIN_URL_PARAM.get('rows')) / CHUNK_SIZE) + 1
+    temp_url_param = copy.deepcopy(MAIN_URL_PARAM)  # otherwise dicts get copied by reference
+    for i in range(0, n):
+        temp_url_param['start'] = i*CHUNK_SIZE
+        print("New Chunk started: [{}/{}] - {} ms".format(i, n-1, round(time.time(), 2)))
+        if i+1 != n:
+            temp_url_param['rows'] = CHUNK_SIZE
+        else:
+            print(MAIN_URL_PARAM.get('rows'))
+            temp_url_param['rows'] = int(int(MAIN_URL_PARAM.get('rows')) % CHUNK_SIZE)
+        print(temp_url_param)
+        temp_json = load_remote_content(MAIN_URL, temp_url_param)
+        data = test_json(temp_json)
+        if data:  # no else required, test_json already gives us an error if something fails
+            traverse_json_data(data)
+            write_statistic()
 
 
 if __name__ == "__main__":
     main()
     print("Overall Executiontime was {} seconds".format(str(round(time.time() - start_time, 3))))
     print("Statistic has {} entries".format(len(statistic)))
+    with open("debug.json", "w") as fw:
+        json.dump(debug_json, fw, indent=4)
